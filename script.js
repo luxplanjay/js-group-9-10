a40cf7e0709b9c61ea416d895b54c3664fd6a599
Alexander Repeta <alexander.repeta@gmail.com>
2018-11-29 21:23:16 +0200

    Added lesson 2

--- script.js
+++ script.js
@@ -1,115 +1,145 @@
 'use strict';
 
-/*
-  Переменные
-*/
-// const message = 'Hello';
-// console.log(message);
-// alert(message);
+// const value = 8;
 
-/*
-  ввод и вывод
- */
-// const isGoing = confirm('Вы точно летите на отдых?');
-// console.log(isGoing);
+// console.log('value > 5 && value < 10: ', value > 5 && value < 10);
+// console.log('value < 5 || value > 10: ', value < 5 || value > 10);
 
-// const number = prompt('Введите число');
-// console.log(typeof number);
+// const value = 10;
 
-/*
-  Операторы сравнения
- */
-// const x = 5;
-// const y = 10;
-// const z = 5;
+// if (value > 5) {
+//   console.log('Больше 5!');
+// } else if (value < 5) {
+//   console.log('Меньше 5!');
+// } else {
+//   console.log('Равно пяти!');
+// }
 
-// console.log('x === z: ', x === z);
-// console.log('x !== z: ', x !== z);
-// console.log('true == 1: ', true == 1);
-// console.log('true === 1: ', true === 1);
+// // else {
+// //   console.log('Меньше 5!');
+// // }
 
-/*
-  Числа
- */
+// console.log('Код после if!');
 
-// const number = prompt('Введите число 5');
+// const value = 5;
+// let message;
 
-// console.log(number);
-// console.log('Ввели ожидаемое число?: ', 5 === Number(number));
+// if (value > 5) {
+//   message = 'Больше 5';
+// } else {
+//   message = 'Меньше 5';
+// }
 
-/*
-  Parse
- */
+// message = value > 5 ? 'Больше 5' : 'Меньше 5';
 
-// const widthInt = '25.75px';
-// const widthFloat = '25.75px';
-// let pixels;
+// if else if ...
+// const userInput = prompt('Введите положительное число!');
+// let message = '';
 
-// pixels = Number.parseInt(widthInt);
-// console.log(pixels);
+// // !null -> !false -> true
+// // алтернатива userInput === null
+// if (!userInput) {
+//   message = 'Была нажата отмена!';
+// } else if (Number(userInput) > 0) {
+//   message = `Ввели число ${Number(userInput)}`;
+// } else {
+//   message = 'Невалидные данные!';
+// }
 
-// pixels = Number.parseFloat(widthFloat);
-// console.log(pixels);
-
-/*
-  NaN
- */
-
-// const userInput = prompt('Введите число');
-
-// const parsedUserInput = Number(userInput);
-
-// console.log('Ввели число?: ', !Number.isNaN(parsedUserInput));
-
-// '555'  -> 555 -> false
-// 'qweqwe' -> NaN -> true
-
-/* Floats */
-
-// console.log(0.1 + 0.2 === 0.3);
-
-// const number = 1.5;
-
-// console.log(Math.round(number));
-
-/*
-  Строки
-*/
-
-// const message = 'Welcome to ';
-// const hotelName = 'Sunlight hotel';
-
-// console.log(message + hotelName);
-
-// console.log(5111 + '5'); // '51115'
-
-// const name = 'Mango is awesome';
-
-// const inLowerCase = name.toLowerCase();
-// const inUpperCase = name.toUpperCase();
-
-// console.log(name);
-// console.log(inLowerCase);
-// console.log(inUpperCase);
-
-// console.log(name.indexOf('awesome'));
-
-// console.log(name.includes('qweqweqwe'));
-
-// Интерполяция
-
-// const guestName = 'Mango';
-// const hotelName = 'Sunrise hotel';
-// const days = 10;
-
-// 'имя добро пожаловать в отель на дней';
-
-// const message =
-//   guestName + ' добро пожаловать в ' + hotelName + ' на ' + days + ' дней';
-
-// const message = `${guestName} добро пожаловать в ${hotelName} на ${days} дней`;
 // console.log(message);
 
-const name = prompt('Введите имя', 'гость');
+// switch
+// let userInput = prompt(
+//   'Варианты доставки: 0 - самовывоз, 1 - курьер, 2 - новая почта',
+// );
+// let message;
 
-console.log(`Добро пожаловать ${name}`);
+// if (!userInput) {
+//   console.log('Отмена!');
+// } else {
+//   userInput = Number(userInput);
+
+//   // if (userInput === 0) {
+//   //   console.log('самовывоз');
+//   // } else if (userInput === 1) {
+//   //   console.log('курьер');
+//   // } else if (userInput === 2) {
+//   //   console.log('новая почта');
+//   // } else {
+//   // неверный ввод
+//   // }
+
+//   switch (userInput) {
+//     case 0:
+//       console.log('самовывоз');
+//       break;
+//     case 1:
+//       console.log('курьер');
+//       break;
+//     case 2:
+//       console.log('новая почта');
+//       break;
+
+//     default:
+//       console.log('неверный ввод');
+//   }
+
+//   console.log('после switch');
+// }
+
+/*
+  Напишите скрипт который:
+
+  - Через prompt cпрашивает 'Введите произвольное число'
+  - Если пользователь нажал Cancel - показывать alert 'Приходите еще!'
+  - Если посетитель вводит число - показывать alert со строкой 'Спасибо!'
+  - Если посетитель вводит что либо другое — показывать alert 'Необходимо было ввести целое число!'
+
+  PS: используйте конструкцию if..else.
+*/
+
+// const userInput = prompt('Введите произвольное число');
+
+// if (!userInput) {
+//   alert('Приходите еще!');
+// } else if (!Number.isNaN(Number(userInput))) {
+//   alert('Спасибо!');
+// } else {
+//   alert('Необходимо было ввести число!');
+// }
+
+// let x = 'qwe';
+// x = Number(x);
+
+// // x = 5 -> false -> !false -> true
+// // x = NaN -> true -> !true -> false
+// const isNumber = !Number.isNaN(x);
+
+// console.log(isNumber);
+
+// const isAdmin = true;
+// const isLoggedIn = false;
+
+// if (isAdmin && isLoggedIn) {
+//   console.log('запускаю!');
+// } else {
+//   console.log('нет прав!');
+// }
+
+// prompt('wewe') и нажали cancel и вернулось null
+// const value = null;
+
+// if (!value) {
+//   console.log('отмена');
+// }
+
+// const isLoggedIn = false;
+
+// if (!isLoggedIn) {
+//   console.log('не залогинен');
+// }
+
+// const price = 10;
+
+// let applesAmount = 50;
+
