25e95b648c6014d281fbbd82d9f69124b2f4848c
Alexander Repeta <alexander.repeta@gmail.com>
2018-12-06 21:35:21 +0200

    Added loops examples

--- script.js
+++ script.js
@@ -1,106 +1,237 @@
 'use strict';
 
-// const names = ['Mango', 'Poly', 'Ajax'];
+// Передача по значению
+// {
+//   let a = 5;
+//   let b = a;
 
-// console.log(names);
-// console.log('length: ', names.length);
+//   console.log('a: ', a);
+//   console.log('b: ', b);
 
-// names[0] = 'Kiwi';
-// console.log(names);
-// console.log('length: ', names.length);
+//   a = 20;
 
-// names[3] = 'Mango';
-// console.log(names);
-// console.log('length: ', names.length);
-
-// console.log(names[600]);
-// console.log('length: ', names.length);
-
-// names[5] = 'Chelsey';
-// console.log(names);
-// console.log('length: ', names.length);
-
-// names.length = 2;
-// console.log(names);
-
-// matrix
-// const matrix = [[1, 2, 3], [4, 5, 6, ['a', 'b', 'c']], [7, 8, 9]];
-// console.log(matrix);
-// console.log(matrix[1][3][1]);
-
-// Split & Joing
-
-// const message = 'Hello JS world';
-// const arr = message.split(' ');
-
-// console.log(arr);
-
-// const newMessage = arr.join(' <-> ');
-// console.log(newMessage);
-
-//  indexOf() и includes()
-// const numbers = [6, 8, 10, 15, 25, 30, 47];
-// const min = numbers[0];
-// const max = numbers[numbers.length - 1];
-
-// const input = prompt(`Угадайте число от ${min} до ${max}`);
-
-// const value = Number(input);
-
-// const isInArray = numbers.includes(value);
-
-// if (isInArray) {
-//   console.log('Угадали!');
-// } else {
-//   console.log('Не угадали!');
+//   console.log('a: ', a);
+//   console.log('b: ', b);
 // }
 
-// push(), pop(), shift(), unshift()
+// Передача по ссылке
+// {
+//   const arrA = ['Mango'];
+//   const arrB = arrA;
 
-// const names = ['Mango', 'Ajax', 'Poly'];
+//   console.log('arrA === arrB: ', arrA === arrB);
 
-// names.push('Kiwi', 123);
-// console.log(names);
+//   console.log('arrA: ', arrA);
+//   console.log('arrB: ', arrB);
 
-// names.pop();
-// console.log(names);
+//   arrA.push('Poly');
+//   console.log('arrA: ', arrA);
+//   console.log('arrB: ', arrB);
 
-// names.shift();
-// console.log(names);
+//   arrB.push('Ajax');
+//   console.log('arrA: ', arrA);
+//   console.log('arrB: ', arrB);
 
-// names.unshift('a', 'b');
-// console.log(names);
+//   console.log('[1,2,3] === [1,2,3]: ', [1, 2, 3] === [1, 2, 3]);
+// }
 
-// Array.slice()
+// Циклы
+// {
+//   let counter = 0;
 
-// const names = ['Mango', 'Poly', 'Ajax', 'Kiwi'];
-// const newNames = names.slice(0, 2);
+//   while (counter < 10) {
+//     console.log('counter: ', counter);
 
-// console.log('newNames: ', newNames);
+//     counter += 1;
+//   }
 
-// Array.splice()
+//   console.log('after while');
+// }
 
-// const names = ['Mango', 'Poly', 'Ajax', 'Kiwi'];
+// {
+//   const names = ['Mango', 'Poly', 'Ajax', 'Kiwi'];
+//   let i = 0;
+//   const max = names.length;
 
-// const deletedEls = names.splice(1, 3);
-// console.log('удаленные элементы: ', deletedEls);
-// console.log(names);
+//   while (i < max) {
+//     console.log('i: ', i);
+//     console.log('names[i]: ', names[i]);
 
-// names.splice(2, 0, 'Chelsey', 'Monkong');
-// console.log(names);
+//     i += 1;
+//   }
+// }
 
-// names.splice(1, 2, 'a', 'b');
-// console.log(names);
+// {
+//   const values = [];
+//   const max = 5;
+//   let counter = 0;
 
-// Array.concat()
+//   while (counter < max) {
+//     let input = prompt('Введи что-то');
 
-// const oldNames = ['Mango', 'Poly'];
-// const newNames = ['Kiwi', 'Ajax'];
+//     values.push(input);
 
-// const allNames = oldNames.concat(newNames);
-// const allNames = [].concat(oldNames, newNames);
+//     // counter = counter + 1;
+//     counter += 1;
+//   }
 
-// console.log('oldNames: ', oldNames);
-// console.log('newNames: ', newNames);
-// console.log('allNames: ', allNames);
+//   console.log('values: ', values);
+// }
+
+// {
+//   let userInput;
+
+//   do {
+//     userInput = prompt('Введите число от 1 до 5');
+
+//     userInput = Number(userInput);
+
+//     console.log(userInput);
+//   } while (userInput < 1 || userInput > 5);
+// }
+
+// for
+
+// {
+//   const names = ['Mango', 'Poly', 'Ajax', 'Kiwi'];
+
+//   for (let i = 0; i < names.length; i += 1) {
+//     console.log('i: ', i);
+//     console.log('names[i]: ', names[i]);
+//   }
+
+//   for (let i = 0; i < names.length; i += 1) {
+//     console.log('i: ', i);
+//     console.log('names[i]: ', names[i]);
+//   }
+// }
+
+// {
+//   const numbers = [98, 4, 76, 5, 27, 1, 8, 2];
+//   let smallestNumber = numbers[0];
+
+//   for (let i = 1; i < numbers.length; i += 1) {
+//     const currentNumber = numbers[i];
+
+//     console.log('i: ', i);
+//     console.log('currentNumber: ', currentNumber);
+//     console.log('smallestNumber: ', smallestNumber);
+
+//     if (currentNumber < smallestNumber) {
+//       smallestNumber = currentNumber;
+//     }
+//   }
+
+//   console.log('smallestNumber: ', smallestNumber);
+// }
+
+// {
+//   const numbers = [98, 4, 76, -20, 5, 27, 1, 8, 2];
+//   let smallestNumber = numbers[0];
+
+//   for (const number of numbers) {
+//     if (number < smallestNumber) {
+//       smallestNumber = number;
+//     }
+//   }
+
+//   console.log(smallestNumber);
+// }
+
+// {
+//   const numbers = [98, 3, 17, 4, 76, -20, 34, 5, 27, 1, 8, 2];
+//   const evenNumbers = [];
+
+//   for (let i = 0, max = numbers.length; i < max; i += 1) {
+//     const currentNumber = numbers[i];
+
+//     if (currentNumber % 2 === 0) {
+//       evenNumbers.push(currentNumber);
+//     }
+//   }
+
+//   console.log(numbers);
+//   console.log(evenNumbers);
+// }
+
+// {
+//   const numbers = [98, 3, 17, 4, 76, -20, 34, 5, 27, 1, 8, 2];
+//   const evenNumbers = [];
+
+//   for (const number of numbers) {
+//     if (number % 2 === 0) {
+//       evenNumbers.push(number);
+//     }
+//   }
+
+//   console.log(numbers);
+//   console.log(evenNumbers);
+// }
+
+// {
+//   const numbers = [1, 2, 3, 4, 5];
+
+//   for (let i = 0; i < numbers.length; i += 1) {
+//     numbers[i] = numbers[i] * 2;
+//   }
+
+//   console.log(numbers);
+// }
+
+// {
+//   const numbers = [1, 2, 3, 4, 5];
+
+//   for (let number of numbers) {
+//     number = number * 2;
+//   }
+
+//   console.log(numbers);
+// }
+
+// {
+//   const clients = ['Mango', 'Poly', 'Ajax'];
+//   const clientNameToFind = 'Poly';
+//   let resultMsg = 'Не нашли!';
+
+//   for (const client of clients) {
+//     if (client === clientNameToFind) {
+//       resultMsg = 'Нашли!';
+//       break;
+//     }
+//   }
+
+//   console.log(resultMsg);
+// }
+
+// Бесконечные циклы
+
+// {
+//   let userInput;
+
+//   while (true) {
+//     userInput = prompt(
+//       'Выберите вариант доставки: 1 - самовывоз, 2 - курьер, 3 - почта',
+//     );
+
+//     if (userInput === null) break;
+
+//     userInput = Number(userInput);
+
+//     if (userInput >= 1 && userInput <= 3) break;
+//   }
+
+//   switch (userInput) {
+//     case 1:
+//       console.log('самовывоз');
+//       break;
+//     case 2:
+//       console.log('курьер');
+//       break;
+//     case 3:
+//       console.log('почта');
+//       break;
+//     default:
+//       console.log('отмена');
+//   }
+// }
 
