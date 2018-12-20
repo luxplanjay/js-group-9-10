037d5dbb3ba75c4847df124bc464220f93648314
Alexander Repeta <alexander.repeta@gmail.com>
2018-12-20 21:29:53 +0200

    Added examples

---
+++ destructuring.js
@@ -0,0 +1,61 @@
+// const user = {
+//   id: 1,
+//   name: 'Mango',
+//   age: 2,
+//   mood: 'happy',
+// };
+
+// const name = user.name;
+// const age = user.age;
+// const mood = user.mood;
+
+// const { name: userName = 'user', age: userAge = 1, mood = 'hello' } = user;
+
+// const message = `Hello ${userName}, you are ${userAge} years old and ${mood}`;
+
+// console.log(message);
+
+// const foo = function(a = 5) {
+//   console.log(a);
+// };
+
+// const renderUserProfile = function({ id, name, age, friendCount, mood }) {
+//   console.log(name);
+// };
+
+// const renderUserProfile = function(user) {
+//   const { id, ...rest } = user;
+
+//   console.log('id: ', id);
+//   console.log(rest);
+// };
+
+// renderUserProfile({
+//   id: 1,
+//   name: 'mango',
+//   age: 2,
+//   friendCount: 23,
+//   mood: 'happy',
+// });
+
+// const colors = [255, 200, 155];
+
+// const r = colors[0];
+// const g = colors[1];
+// const b = colors[2];
+
+// const [r, g, b, alfa = 0.3] = colors;
+
+// console.log(r);
+// console.log(g);
+// console.log(b);
+// console.log(alfa);
+
+// const form = document.querySelector('.form');
+
+// console.dir(form.elements);
+
+// const [input, textarea] = form.elements;
+
+// console.log(input);
+// console.log(textarea);
---
+++ rest-spread.js
@@ -0,0 +1,93 @@
+'use strict';
+
+// const defaultSettings = {
+//   theme: 'dark',
+//   showSidebar: true,
+//   notifications: true,
+// };
+
+// const userSettings = {
+//   showSidebar: false,
+//   notifications: false,
+// };
+
+/*
+{
+  theme: 'dark',
+   showSidebar: false,
+  notifications: false,
+}
+*/
+
+// const updatedSettings = Object.assign({}, defaultSettings, userSettings);
+
+// console.log(updatedSettings);
+// const obj1 = { a: 1, b: 1, e: { inner: 10 } };
+// const obj2 = { a: 2, c: 3 };
+// const obj = Object.assign({}, obj1, obj2);
+
+// obj1.e.inner = 500;
+
+// console.log('obj: ', obj);
+// console.log('obj1: ', obj1);
+
+// const original = [{}, {}, {}];
+
+// const copy = [...original];
+
+// console.log(copy === original);
+
+// console.log(copy[0] === original[0]);
+
+// original[0].name = 'new stuff';
+
+// console.log(copy);
+
+// const houses = ['Arryn', 'Frey', 'Greyjoy', 'Stark', 'Lannister', 'Tyrell'];
+
+// const completeHouses = [...houses, 'Targaryen'];
+
+// console.log(completeHouses);
+
+// const inDebt = [...completeHouses.slice(0, 4), ...completeHouses.slice(5)];
+
+// console.log(inDebt);
+
+// const defaultSettings = {
+//   theme: 'dark',
+//   showSidebar: true,
+//   notifications: true,
+// };
+
+// const userSettings = {
+//   showSidebar: false,
+//   notifications: false,
+// };
+
+// const updatedSettings = Object.assign({}, defaultSettings, userSettings);
+
+// const updatedSettings = {
+//   ...defaultSettings,
+//   ...userSettings,
+//   name: 'Eric',
+// };
+
+// console.log(updatedSettings);
+
+// const foo = function(a, b, ...args) {
+//   console.log('a: ', a);
+//   console.log('b: ', b);
+
+//   console.log(args);
+// };
+
+// foo(1, 2, 3, 4, 5);
+
+// const obj = {
+//   a: 1,
+//   b: 2,
+// };
+
+// console.log('c' in obj);
+// console.log(obj.hasOwnProperty('c'));
+
