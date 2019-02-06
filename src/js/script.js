ec26a5039fe48ac6cbede30a120faf9b798ff77f
Alexander Repeta <axzerk@gmail.com>
2019-02-06 20:33:52 +0200

    Adds modal window

--- src/js/script.js
+++ src/js/script.js
@@ -1,138 +1,45 @@
 'use strict';
 
-const buttonActions = {
-  EDIT: 'edit',
-  DELETE: 'delete',
-};
-
-const data = [
-  {
-    id: 'id-1',
-    body:
-      'Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam. Fusce ac felis sit amet ligula pharetra condimentum. Phasellus dolor.',
-  },
-  {
-    id: 'id-2',
-    body:
-      'Ut id nisl quis enim dignissim sagittis. Quisque malesuada placerat nisl. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam.',
-  },
-  {
-    id: 'id-3',
-    body:
-      'Quisque malesuada placerat nisl. Fusce convallis metus id felis luctus adipiscing.',
-  },
-];
-
-class Model {
-  static generateUniqueId = () =>
-    Math.random()
-      .toString(36)
-      .substring(2, 15) +
-    Math.random()
-      .toString(36)
-      .substring(2, 15);
-
-  constructor(items = []) {
-    this._items = items;
-  }
-
-  save(text) {
-    const newItem = {
-      id: Model.generateUniqueId(),
-      body: text,
-    };
-
-    this._items.push(newItem);
-
-    return newItem;
-  }
-
-  filter(query = '') {
-    return this._items.filter(item =>
-      item.body.toLowerCase().includes(query.toLowerCase()),
-    );
-  }
-}
-
-const model = new Model(data);
-
-// Refs
 const refs = {
-  list: document.querySelector('.list'),
-  editor: document.querySelector('.editor'),
-  filter: document.querySelector('.filter'),
+  openModalBtn: document.querySelector('button[data-action="open-modal"'),
+  closeModalBtn: document.querySelector('button[data-action="close-modal"'),
+  modalBackdrop: document.querySelector('.js-backdrop'),
+  body: document.body,
 };
 
-// UI
-const createListItem = ({ id, body }) => {
-  const listItem = document.createElement('li');
-  listItem.classList.add('list-item');
-  listItem.dataset.id = id;
-
-  const text = document.createElement('p');
-  text.classList.add('text');
-  text.textContent = body;
-
-  const actionsContainer = document.createElement('div');
-  actionsContainer.classList.add('actions');
-
-  const editButton = document.createElement('button');
-  editButton.classList.add('btn');
-  editButton.textContent = 'Edit';
-  editButton.dataset.action = buttonActions.EDIT;
-
-  const deleteButton = document.createElement('button');
-  deleteButton.classList.add('btn');
-  deleteButton.textContent = 'Delete';
-  deleteButton.dataset.action = buttonActions.DELETE;
-
-  actionsContainer.append(editButton, deleteButton);
-  listItem.append(text, actionsContainer);
-
-  return listItem;
+const openModal = () => {
+  refs.body.classList.add('modal-visible');
+  window.addEventListener('keydown', handleKeyPress);
 };
 
-const renderListItems = (listRef, items) => {
-  const listItems = items.map(item => createListItem(item));
-
-  listRef.innerHTML = '';
-  listRef.append(...listItems);
+const closeModal = () => {
+  refs.body.classList.remove('modal-visible');
+  window.removeEventListener('keydown', handleKeyPress);
 };
 
-const addItemToList = (listRef, item) => {
-  const listItem = createListItem(item);
+const handleKeyPress = e => {
+  console.log(e);
 
-  listRef.appendChild(listItem);
-};
-
-// Handlers
-const handleEditorSubmit = event => {
-  event.preventDefault();
-
-  const [input] = event.currentTarget.elements;
-  const inputValue = input.value;
-
-  if (inputValue.trim() === '') {
-    return alert('Ты ничего не ввел!');
+  if (e.code !== 'Escape') {
+    return;
   }
 
-  const savedItem = model.save(inputValue);
-
-  addItemToList(refs.list, savedItem);
-
-  event.currentTarget.reset();
+  closeModal();
 };
 
-const handleFilterChange = event => {
-  console.log(event.target.value);
+const handleBackdropClick = ({ target, currentTarget }) => {
+  // target - самый глубокий элемент на котором произошло событие
+  // console.log('target: ', target);
+  // currentTarget - элемент на котором сработал слушатель
+  // console.log('currentTarget: ', currentTarget);
 
-  const filteredItems = model.filter(event.target.value);
+  if (target !== currentTarget) {
+    return;
+  }
 
-  renderListItems(refs.list, filteredItems);
+  closeModal();
 };
 
-renderListItems(refs.list, data);
-
-// Listeners
-refs.editor.addEventListener('submit', handleEditorSubmit);
-refs.filter.addEventListener('input', handleFilterChange);
+refs.modalBackdrop.addEventListener('click', handleBackdropClick);
+refs.openModalBtn.addEventListener('click', openModal);
+refs.closeModalBtn.addEventListener('click', closeModal);
